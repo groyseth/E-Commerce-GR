7ead0035e1fd206619af37a4683b90822396633e
Grant Royseth <groyseth@gmail.com>
2022-01-30 22:04:23 -0600

    work on post product

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category //done
 
-// Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  
+});
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag,
+   {through: ProductTag} );
+// Categories have many Products// done
+
+// Products belongToMany Tags (through ProductTag)// done
+Tag.belongsToMany(Product,
+   {through: ProductTag} );
+
+// Tags belongToMany Products (through ProductTag)// done
 
 module.exports = {
   Product,
